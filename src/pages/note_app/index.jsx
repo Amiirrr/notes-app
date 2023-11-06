6aae2d201825b96d2b5302d3e6266d67cb73fd94
amiirrr <amirkholiluddinismail22@gmail.com>
2023-11-06 08:24:08 +0700

    adding message 'empty' when notes is empty

--- src/pages/note_app/index.jsx
+++ src/pages/note_app/index.jsx
@@ -55,11 +55,19 @@ const NoteApp = () => {
             </button>
             <div className="note-body flex flex-row flex-wrap gap-4 justify-center py-10 bg-white rounded-r-2xl rounded-b-2xl shadow-md max-w-7xl max-h-100">
                 {
-                    selected === 'notes' ?
-                        <Notes note={note} showDropdown={showDropdown} selectedNoteId={selectedNoteId} handleArchived={handleArchived} handleDelete={handleDelete} Dots={Dots} handleToggleDropdown={handleToggleDropdown} />
-
-                        : selected === 'archived' ?
-                            <Archived note={note} showDropdown={showDropdown} selectedNoteId={selectedNoteId} handleArchived={handleArchived} handleDelete={handleDelete} Dots={Dots} handleToggleDropdown={handleToggleDropdown} />
+                    selected === 'notes' ? (
+                        note.length > 0 ? (
+                            <Notes note={note} showDropdown={showDropdown} selectedNoteId={selectedNoteId} handleArchived={handleArchived} handleDelete={handleDelete} Dots={Dots} handleToggleDropdown={handleToggleDropdown} />
+                        ) : (
+                            <p className='text-gray-400'>notes is empty</p>
+                        )
+                    )
+                        : selected === 'archived' ? (
+                            note.length > 0 ? (
+                                <Archived note={note} showDropdown={showDropdown} selectedNoteId={selectedNoteId} handleArchived={handleArchived} handleDelete={handleDelete} Dots={Dots} handleToggleDropdown={handleToggleDropdown} />
+                            ) : (
+                                <p className='text-gray-400'>archive is empty</p>
+                            ))
                             : null
                 }
             </div>
